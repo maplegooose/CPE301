aa8d98b40b7cf91453a89006b6877dd7a9bd3a70
Tyler Truong <109112722+maplegooose@users.noreply.github.com>
2025-12-12 16:30:02 -0800

    Changing states with buttons
    start button now turns green LED on
    stop button now turns yellow LED on

--- Truong_CPE301_FinalProject.ino
+++ Truong_CPE301_FinalProject.ino
@@ -1,16 +1,36 @@
+
 void setup() {
-  DDRB = DDRB | 0b11110000;
-  /*
+  DDRB = 0b11110000;
+  /* designates as outputs
   bit 7: RED LED
   bit 6: YELLOW LED
   bit 5: GREEN LED
   bit 4: BLUE LED
   */
+  PORTB = 0b10000000; //starting in disabled
+  DDRD = DDRD & 0b11111100;
+  PORTD = DDRD & 0B11111100;
+  /* designates as inputs
+  bit 5: stop button
+  bit 4: start button
+  */
+
+  //setup for interrupts
+  SREG = 0b10000000; //globally enables interrupts
+  EICRA = 0b00001010; //triggers flag on falling edge for INT1:0
+  EIMSK = 0b00000011; //enables interrupts for INT1:0
 }
 
 void loop() {
-  PORTB = PORTB | 0b11110000;
-  delay(100);
-  PORTB = PORTB & 0b00001111;
-  delay(100);
+
+}
+
+//switches to disabled state
+ISR(INT0_vect){
+  PORTB = 0b01000000;
+}
+
+//switches to idle state
+ISR(INT1_vect){
+  PORTB = 0b00100000;
 }
