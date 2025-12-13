06c11df99377c6c6987ea626942c3406ad877086
Tyler Truong <109112722+maplegooose@users.noreply.github.com>
2025-12-13 02:16:13 -0800

    Water Sensor
    water sensor has been implemented using gipo level coding for initialization and water level reading

--- Truong_CPE301_FinalProject.ino
+++ Truong_CPE301_FinalProject.ino
@@ -2,6 +2,8 @@
 #include "TinyStepper.h"
 #include "LiquidCrystal.h"
 
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
 //creating objects
 LiquidCrystal lcd(50, 51, 31, 30, 29, 28);
 DHT dht(2, DHT11);
@@ -40,20 +42,22 @@ void setup() {
 
   PORTB = 0b01000000; //starting in idle
 
+  adc_init();
+
   dht.begin();
   vent.Enable();
   lcd.begin(16, 2);
-  Serial.begin(9600);
+  Serial.begin(9600); //REMOVE
 }
 
 void loop() {
   int state = PORTB;
 
   if(state != 0b01000000){
+    //printing data from dht onto LCD
     float temp = dht.readTemperature();
     float humidity = dht.readHumidity();
     
-    //printing stuff for LCD
     lcd.print("temp: ");
     lcd.print(temp);
     lcd.print(" C");
@@ -62,18 +66,20 @@ void loop() {
     lcd.print(humidity);
     lcd.print("%");
 
+    //checks for button to move the vent
     int moveState1 = PINH;
     int moveState2 = PINH;
 
     if((moveState1 & 0b00010000) && 0b00010000){
       vent.Move(-15);
-      Serial.println("left");
     }else if((moveState2 & 0b00001000) && 0b00001000){
       vent.Move(15);
-      Serial.println("right");
     }
 
     analogWrite(9, 120); //generates low  base voltage for transistor to turn on fan motor
+
+    int waterLevel = adc_read(0);
+    Serial.println(waterLevel);
 
   }
   else{
@@ -92,4 +98,40 @@ ISR(INT0_vect){
 //switches to idle state
 ISR(INT1_vect){
   PORTB = 0b00100000;
+}
+
+void adc_init() //initializes ADC for water sensor
+{
+  ADCSRA |= (1 << 7);  
+  ADCSRA &= ~(1 << 5); 
+  ADCSRA &= ~(1 << 3); 
+  ADCSRA |= (1 << 2);  
+  ADCSRA |= (1 << 1);
+  ADCSRA |= (1 << 0);
+
+  ADCSRB &= ~(1 << 3); 
+  ADCSRB &= 0xF8;
+
+  ADMUX &= ~(1 << 7);  
+  ADMUX |= (1 << 6);   
+  ADMUX &= ~(1 << 5);  
+  ADMUX &= 0xE0;
+}
+
+unsigned int adc_read(unsigned char adc_channel_num) //work with channel 0
+{
+  ADMUX &= 0xE0;
+  ADCSRB &= ~(1 << 3);
+
+  if(adc_channel_num > 7){
+    adc_channel_num = adc_channel_num -8;
+      ADCSRB |= (1 << 3);
+  }
+
+  ADMUX |= adc_channel_num;
+  ADCSRA |= (1 << 6);
+
+  while((ADCSRA & (1 <<6)) != 0);
+  
+  return *my_ADC_DATA;
 }
