4f3acc79224a3c799203eba2693e0210781d9878
Tyler Truong <109112722+maplegooose@users.noreply.github.com>
2025-12-12 17:33:08 -0800

    Temperature Implementation
    temperature is now sent to serial monitor when state is not in disabled

--- Truong_CPE301_FinalProject.ino
+++ Truong_CPE301_FinalProject.ino
@@ -1,3 +1,7 @@
+#include "DHT.h" 
+
+//creating objects
+DHT dht(2, DHT11);
 
 void setup() {
   DDRB = 0b11110000;
@@ -7,22 +11,40 @@ void setup() {
   bit 5: GREEN LED
   bit 4: BLUE LED
   */
-  PORTB = 0b10000000; //starting in disabled
   DDRD = DDRD & 0b11111100;
   PORTD = DDRD & 0B11111100;
   /* designates as inputs
   bit 5: stop button
   bit 4: start button
   */
+  DDRE = DDRE | 0b00010000; 
+  PORTE = PORTE | 0b00010000;
+  /* designates as input
+  bit 4: DHT sensor
+  */
 
   //setup for interrupts
   SREG = 0b10000000; //globally enables interrupts
   EICRA = 0b00001010; //triggers flag on falling edge for INT1:0
   EIMSK = 0b00000011; //enables interrupts for INT1:0
+
+  PORTB = 0b01000000; //starting in idle
+
+  dht.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
+  int state = PORTB;
 
+  if(state != 0b01000000){
+    float temp = dht.readTemperature();
+    Serial.println(temp);
+  }
+  else{
+    Serial.println("idle");
+  }
+  delay(2000);
 }
 
 //switches to disabled state
