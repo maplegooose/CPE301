c5b66165554fbbc72ee6b293109c494bdd7e5a12
Tyler Truong <109112722+maplegooose@users.noreply.github.com>
2025-12-13 21:12:25 -0800

    DHT Sensor
    added and implemented DHT sensor

--- Truong_CPE301_FinalProject2_copy_20251213202955.ino
+++ Truong_CPE301_FinalProject2_copy_20251213202955.ino
@@ -1,3 +1,8 @@
+//libraries
+#include "DHT.h" 
+
+//creating objects
+DHT dht(9, DHT11);
 
 void setup() {
   DDRB = 0b11110000;
@@ -8,18 +13,38 @@ void setup() {
   bit 4: BLUE LED
   */
   PORTB = 0b01000000; //starting in idle
-  DDRG & 0b11011111; //sets pin 4 (stop button)
+
+  DDRG & 0b11011111; //calls pin 4 an input (stop button)
   PORTG & 0b11011111;
 
   //setup for interrupts
   SREG = 0b10000000; //globally enables interrupts
-  attachInterrupt(digitalPinToInterrupt(2), START, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), START, FALLING); //setting up START ISR
 
+  dht.begin(); //starting the dht sensor
   Serial.begin(9600);
 }
 
 void loop() {
+  while(PORTB == 0b00100000){
+    //Serial.println("State: idle");
 
+    float temp = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    Serial.print("temp: ");
+    Serial.println(temp);
+    Serial.print("humidity ");
+    Serial.println(humidity);
+
+    //pressing stop button returns state to disabled
+    int stopState = PING;
+    if(PING == 0){
+      PORTB = PORTB << 1;
+    }
+  }
+  Serial.println("State: disabled");
+  //outside of while loop, disabled state
 }
 
 //switches to start state
